c8a5959f62f883c141da013882375d97a42636e4
Israel de Lamo <i.delamo@gmail.com>
2021-01-30 10:56:21 +0100

    Update esp32-cayenne-ds18b20.ino
    Añadido como actuador el BUILTINLED

--- esp32-cayenne-ds18b20.ino
+++ esp32-cayenne-ds18b20.ino
@@ -20,6 +20,8 @@ float temperatureC = 0.0;
 // WiFi network info.
 char ssid[] = ""; //aqui va tu ssid
 char wifiPassword[] = ""; //aqui va tu pass
+const int outputLED = LED_BUILTIN;
+
 
 // Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
 char username[] = "";
@@ -31,6 +33,7 @@ unsigned long lastMillis = 0;
 void setup() {
 	Serial.begin(115200);
  sensors.begin(); //para dallas 18d20
+	pinMode(outputLED, OUTPUT); //defino como salida el led
 	Cayenne.begin(username, password, clientID, ssid, wifiPassword);
 }
 
@@ -45,22 +48,25 @@ void loop() {
   
 }
 
-// Default function for sending sensor data at intervals to Cayenne.
-// You can also use functions for specific channels, e.g CAYENNE_OUT(1) for sending channel 1 data.
-CAYENNE_OUT_DEFAULT()
-{
-	// Write data to Cayenne here. This example just sends the current uptime in milliseconds on virtual channel 0.
-	//Cayenne.virtualWrite(0, millis());
-	// Some examples of other functions you can use to send data.
-	Cayenne.celsiusWrite(1, temperatureC);
-	//Cayenne.luxWrite(2, 700);
-	//Cayenne.virtualWrite(3, 50, TYPE_PROXIMITY, UNIT_CENTIMETER);
-}
-
 // Default function for processing actuator commands from the Cayenne Dashboard.
 // You can also use functions for specific channels, e.g CAYENNE_IN(1) for channel 1 commands.
-CAYENNE_IN_DEFAULT()
+CAYENNE_IN(2)
 {
-	CAYENNE_LOG("Channel %u, value %s", request.channel, getValue.asString());
-	//Process message here. If there is an error set an error message using getValue.setError(), e.g getValue.setError("Error message");
+  
+  CAYENNE_LOG("Got a value: %s", getValue.asStr());
+  int i = getValue.asInt();
+  
+  if (i == 0)
+  {
+    Serial.println(i);
+    digitalWrite(outputLED, HIGH);
+  }
+  else
+  {
+    Serial.println(i);
+    digitalWrite(outputLED, LOW);
+  }  
+  
+  
+  //Process message here. If there is an error set an error message using getValue.setError(), e.g getValue.setError("Error message");
 }
